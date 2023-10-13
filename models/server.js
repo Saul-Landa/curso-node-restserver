ab2fca5056387f4505c93dd5eaa642ff70e9a8a7
Saúl Landa García <75047785+Saul-Landa@users.noreply.github.com>
2023-10-12 21:05:25 -0600

    Default port updated

--- models/server.js
+++ models/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 class Server {
     constructor() {
         this.app = express();
-        this.port = process.env.port;
+        this.port = process.env.port || 3000;
         this.usuarioPath = '/api/usuarios';
 
         // Middlewares
